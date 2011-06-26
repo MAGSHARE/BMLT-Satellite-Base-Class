f4713a3ec71c43b57bd930d97f286771f9990fe4
Chris M <webservant@magnaws.org>
2011-06-26 14:50:23 -0400

    Tweaked the comments to better reflect the file.

--- bmlt-unit-test-satellite-plugin.php
+++ bmlt-unit-test-satellite-plugin.php
@@ -1,29 +1,29 @@
 <?php
 /****************************************************************************************//**
-*   \file   bmlt-drupal-satellite-plugin.php                                                *
+*   \file   bmlt-unit-test-satellite-plugin.php                                             *
 *                                                                                           *
 *   \brief  This is a standalone unit test plugin of a BMLT satellite client.               *
-*   \version 1.0.8                                                                          *
+*   \version 1.1.0                                                                          *
 *                                                                                           *
-    This file is part of the BMLT Common Satellite Base Class Project. The project GitHub
-    page is available here: https://github.com/MAGSHARE/BMLT-Common-CMS-Plugin-Class
-    
-    This file is part of the Basic Meeting List Toolbox (BMLT).
-    
-    Find out more at: http://magshare.org/bmlt
-    
-    BMLT is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-    
-    BMLT is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-    
-    You should have received a copy of the GNU General Public License
-    along with this code.  If not, see <http://www.gnu.org/licenses/>.
+*   This file is part of the BMLT Common Satellite Base Class Project. The project GitHub   *
+*   page is available here: https://github.com/MAGSHARE/BMLT-Common-CMS-Plugin-Class        *
+*                                                                                           *
+*   This file is part of the Basic Meeting List Toolbox (BMLT).                             *
+*                                                                                           *
+*   Find out more at: http://magshare.org/bmlt                                              *
+*                                                                                           *
+*   BMLT is free software: you can redistribute it and/or modify                            *
+*   it under the terms of the GNU General Public License as published by                    *
+*   the Free Software Foundation, either version 3 of the License, or                       *
+*   (at your option) any later version.                                                     *
+*                                                                                           *
+*   BMLT is distributed in the hope that it will be useful,                                 *
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of                          *
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                           *
+*   GNU General Public License for more details.                                            *
+*                                                                                           *
+*   You should have received a copy of the GNU General Public License                       *
+*   along with this code.  If not, see <http://www.gnu.org/licenses/>.                      *
 ********************************************************************************************/
 
 define ( '_DEBUG_MODE_', 1 ); //Uncomment for easier JavaScript debugging.
