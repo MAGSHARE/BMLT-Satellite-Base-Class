cdb879ae9a718fb212498b29c6957be32d1ad645
chris@littlegreenviper.com <webservant@magnaws.org>
2013-01-27 09:24:41 -0500

    Added a larger radius selection at the end of the list for larger (more rural) areas.

--- lang/lang_sv.php
+++ lang/lang_sv.php
@@ -220,7 +220,7 @@ class BMLT_Localized_BaseClass
     static  $local_nouveau_default_duration = '1:30';
     static  $local_nouveau_noon = 'Noon';
     static  $local_nouveau_midnight = 'Midnight';
-    static  $local_nouveau_advanced_map_radius_value_array = "0.25, 0.5, 1.0, 2.0, 5.0, 10.0, 15.0, 20.0, 50.0, 100.0";
+    static  $local_nouveau_advanced_map_radius_value_array = "0.25, 0.5, 1.0, 2.0, 5.0, 10.0, 15.0, 20.0, 50.0, 100.0, 200.0";
     static  $local_nouveau_meeting_details_link_title = 'Get more details about this meeting.';
 
     static  $local_nouveau_single_formats_label = 'Meeting Formats:';
